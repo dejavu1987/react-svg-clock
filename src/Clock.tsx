12d6266f5015388acb251368de14db9a144e6405
Anil Maharjan <anil@anilmaharjan.com.np>
2021-05-05 20:44:28 +0200

    Tutorial #2 Get angle from ratios

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -4,10 +4,18 @@ import "./styles.css";
 import useInterval from "./useInterval";
 
 
+const getAngleFromRatio = (ratio: number): number => {
+  return ratio * 2 * Math.PI;
+};
+
 export default function Clock() {
   const [hour, setHour] = useState<number>(0);
   const [min, setMin] = useState<number>(0);
   const [sec, setSec] = useState<number>(0);
+
+  const [hourAngle, setHourAngle] = useState<number>(0);
+  const [minAngle, setMinAngle] = useState<number>(0);
+  const [secAngle, setSecAngle] = useState<number>(0);
 
 
   useInterval(() => {
@@ -20,6 +28,10 @@ export default function Clock() {
     setHour(h > 11 ? h - 12 : h);
     setMin(m);
     setSec(s);
+
+    setHourAngle(getAngleFromRatio(h / 12));
+    setMinAngle(getAngleFromRatio(m / 60));
+    setSecAngle(getAngleFromRatio(s / 60));
     
   }, 1000);
 
@@ -33,6 +45,9 @@ export default function Clock() {
       <h3>
         Hour: {hour}, Minutes: {min}, Seconds: {sec}
       </h3>
+      {hourAngle}
+      {minAngle}
+      {secAngle}
     </div>
   );
 }
